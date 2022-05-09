1ac209b5f9f78487cdd95d31dd2e6e3f771c69d1
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-09 00:11:21 -0700

    Repeat until Win

--- snackAndLadder/Program.cs
+++ snackAndLadder/Program.cs
@@ -4,56 +4,60 @@ namespace snakeLadder
 {
     class program
     {
-        //**Declaring constant
+        //**Declaring constant for Logic-2**
         public const int noPlay = 0;
         public const int ladder = 1;
         public const int snake = 2;
         static void Main(string[] args)
         {
             int playerPosition = 0, rollDice, playerOption, listIndex;
-            Console.WriteLine(playerPosition);
             Random random = new Random();
-            rollDice = random.Next(1, 7);
-            Console.WriteLine("Number appered on dice is {0} ", rollDice);
-
-            //Player option Generation "Array" Logic-1
             string[] choiceArray = { "noPlay", "ladder", "Snake" };
-            listIndex = random.Next(choiceArray.Length);
-            Console.WriteLine("Player option is: {0}", choiceArray[listIndex]);
+            while (playerPosition < 100)
+            {
+                //Dice rolling logic
+                rollDice = random.Next(1, 7);
+                Console.WriteLine("Number appered on dice is : {0} ", rollDice);
+
+                //Player option Generation "Array" Logic-1
+                listIndex = random.Next(choiceArray.Length);
+                Console.WriteLine("Player option is: {0}", choiceArray[listIndex]);
 
 
-            //*******Player options generating "Random" logic-2********
-            /*playerOption = random.Next(1, 3);
-            switch (playerOption)
-            {
-                case noPlay:
-                    Console.WriteLine("Player option is : No Play");
-                    break;
-                case ladder:
-                    Console.WriteLine("Player option is : Ladder");
-                    break;
-                case snake:
-                    Console.WriteLine("Player Option is : snake");
-                    break ;      
-            }*/
-
-            //Action according to player option
-            if (choiceArray[listIndex] == "ladder")
-            {
-                playerPosition += rollDice;
-            }
-            else if (choiceArray[listIndex] == "snake")
-            {
-                if (playerPosition < rollDice)
+                //*******Player options generating "Random" logic-2********
+                /*playerOption = random.Next(1, 3);
+                switch (playerOption)
                 {
-                    playerPosition = 0;
-                }
-                else
+                    case noPlay:
+                        Console.WriteLine("Player option is : No Play");
+                        break;
+                    case ladder:
+                        Console.WriteLine("Player option is : Ladder");
+                        break;
+                    case snake:
+                        Console.WriteLine("Player Option is : snake");
+                        break ;      
+                }*/
+
+                //Action according to player option
+                if (choiceArray[listIndex] == "ladder")
                 {
-                    playerPosition -= rollDice;
+                    playerPosition += rollDice;
                 }
+                else if (choiceArray[listIndex] == "snake")
+                {
+                    if (playerPosition < rollDice)
+                    {
+                        playerPosition = 0;
+                    }
+                    else
+                    {
+                        playerPosition -= rollDice;
+                    }
+                }
+                Console.WriteLine("Player position is : {0}", playerPosition);
             }
-            Console.WriteLine("Player position is : {0}", playerPosition);
+            Console.WriteLine("Last player position is: {0}", playerPosition);
         }
     }
 }
