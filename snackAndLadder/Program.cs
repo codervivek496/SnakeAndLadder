af86e498421a0b023968b713823b2f60e4f9f932
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-08 23:01:28 -0700

    Dice Rolled

--- snackAndLadder/Program.cs
+++ snackAndLadder/Program.cs
@@ -6,8 +6,11 @@ namespace snakeLadder
     {
         static void Main(string[] args)
         {
-            int playerPosition = 0;
+            int playerPosition = 0, rollDice;
             Console.WriteLine(playerPosition);
+            Random random = new Random();
+            rollDice = random.Next(1, 7);
+            Console.WriteLine("Number appered on dice is {0} ", rollDice);
         }
     }
 }
